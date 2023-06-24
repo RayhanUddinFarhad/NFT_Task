134dd56a8c7bb8630a9b5f7926f33949530bc535
ITP <RayhanUddinFarhad11@gmail.com>
2023-06-24 21:47:17 +0600

    Banner design almost completed

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import img1 from '../../../assets/Images/BannerImage-1.png'
+import img2 from '../../../assets/Images/BannerImage-2.png'
+import img3 from '../../../assets/Images/BannerImage-3.png'
+import person from '../../../assets/Images/person1.png'
+
+const Banner = () => {
+    return (
+        <div>
+            <div className='lg:flex items-center justify-center mt-10'>
+                <div className='space-y-5 max-w-3xl'>
+                    <h1 className='text-5xl font-extrabold uppercase'>Discover, And Collect Digital Art NFTS</h1>
+                    <p>Digital marketplace for crypto collectibles and non-fungible tokens (NFTs). Buy, Sell, and discover exclusive digital assets.</p>
+                    <button className='bg-[#3D00B7] text-white py-3 px-5 rounded-3xl font-semibold'>Explore more</button>
+
+
+                    <div className='flex space-x-5'>
+
+                        <div>
+                            <h1 className='text-5xl font-extrabold'>98k+</h1>
+                            <p>Artwork</p>
+                        </div>
+                        <div>
+                            <h1 className='text-5xl font-extrabold'>12k+</h1>
+                            <p>Auction</p>
+                        </div>
+                        <div>
+                            <h1 className='text-5xl font-extrabold'>15k+</h1>
+                            <p>Artist</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className='relative max-w-max mr-5'>
+                    <div className='absolute z-10'>
+                        <div className='relative'>
+                            <img src={img1} alt='' />
+                            <div className='absolute top-5 left-5 text-white font-semibold space-y-2'>
+
+                                <h3 className='text-lg'>Abstr Gradient NFT</h3>
+                                <div className='flex items-center space-x-2'>
+                                    <img className='w-8' src={person} alt="" />
+                                    <p>Arkhan17</p>
+
+                                </div>
+                            </div>
+
+
+                            <div className='absolute bottom-5  w-full'>
+
+                                <div className='bg-indigo-400 bg-opacity-50 px-5 p-2 rounded-lg mx-5'>
+
+                                    <div className='flex justify-between text-white '>
+
+                                        <div>
+
+                                            <p className='text-sm'>Current Bid</p>
+                                            <p>0.25 ETH</p>
+                                        </div>
+
+                                        <div>
+
+                                            <p className='text-wm'>Ends in</p>
+                                            <p>12h 43m 43s</p>
+                                        </div>
+                                    </div>
+
+
+                                </div>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div className='relative'>
+                        <img className='absolute z-0 bottom-0 left-7 transition-transform ' src={img2} alt='' />
+                        <img className='relative -z-10 bottom-0 left-12 transition-transform ' src={img3} alt='' />
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/layout/Main.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Banner from '../components/Home/Banner/Banner';
+import Navbar from '../Shared/Navbar';
+
+const Main = () => {
+    return (
+        <>
+
+        <Navbar></Navbar>
+
+        <Banner></Banner>
+        
+        </>
+    );
+};
+
+export default Main;
